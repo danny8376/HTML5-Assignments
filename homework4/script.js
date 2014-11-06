1a0adc798c775dfc491ab2728af3747de348309d
DannyAAM <danny8376+code@gmail.com>
2014-11-06 23:25:13 +0800

    ablity to change page OwO

---
+++ homework4/script.js
@@ -0,0 +1,37 @@
+document.addEventListener('touchstart', handleTouchStart, false);       
+document.addEventListener('touchmove', handleTouchMove, false);
+
+document.addEventListener('mousedown', handleMouseDown, false);       
+document.addEventListener('mousemove', handleMouseMove, false);
+
+var dragStartPoint = null;
+
+function handleTouchStart(evt) {
+    dragStartPoint = evt.touches[0];
+};
+
+function handleTouchMove(evt) {
+    if (! dragStartPoint) return;
+    changePage(dragStartPoint.clientX - evt.touches[0].clientX);
+    dragStartPoint = null;
+};
+
+function handleMouseDown(evt) {
+    dragStartPoint = evt;
+};
+
+function handleMouseMove(evt) {
+    if (! dragStartPoint) return;
+    changePage(dragStartPoint.clientX - evt.clientX);
+    dragStartPoint = null;
+};
+
+
+function changePage(way) {
+	var pane = document.getElementById("pane");
+    if (way > 0) { // left swipe - to right page
+    	pane.style.marginLeft = '-100%';
+    } else { // right swipe - to left page
+    	pane.style.marginLeft = '0';
+    }
+}
